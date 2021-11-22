3a0958f544039aa2bfa0f62a9eed3eaa67136f94
Ford Laturnas <82431417+fordlaturnas@users.noreply.github.com>
2021-11-22 10:36:28 +0800

    fix hands

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
 const toggle = document.querySelector('.toggle')
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-const month = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
-    if(html.classList.contains('dark')){
+    if (html.classList.contains('dark')) {
         html.classList.remove('dark')
         e.target.innerHTML = 'Dark mode'
     } else {
@@ -21,28 +20,28 @@ toggle.addEventListener('click', (e) => {
 })
 
 function setTime() {
-    const time = new Date()
-    const months = time.getMonth()
+    const time = new Date();
+    const month = time.getMonth()
     const day = time.getDay()
     const date = time.getDate()
     const hours = time.getHours()
-    const hourForClock = hours % 12
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
     const minutes = time.getMinutes()
     const seconds = time.getSeconds()
     const ampm = hours >= 12 ? 'PM' : 'AM'
 
-    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hourForClock, 0, 11, 0, 360)}deg)`
-    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
-    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
 
-    timeEl.innerHTML = `${hourForClock}:${minutes < 10 ? `0${minutes}` :minutes } ${ampm}`
-    dateEl.innerHTML = `${days[day]}, ${month[months]} <span class="circle">${date}</span>`
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
 
-const scale = (number, inMin, inMax, outMin, outMax) => {
-    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin; 
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-
 
 setTime()
 
